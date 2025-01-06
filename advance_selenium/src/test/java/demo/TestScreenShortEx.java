1084dba5c97a3915942a29739d53392dc4ffc5ba
PoojaMSunil <poojamalle60@gmail.com>
2025-01-06 08:38:02 +0530

    pushing again

--- advance_selenium/src/test/java/demo/TestScreenShortEx.java
+++ advance_selenium/src/test/java/demo/TestScreenShortEx.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestScreenShortEx {
 
+	//pushing
 	//pushing after craeting brach
 	// eng2
 	public static void main(String[] args) throws Throwable {
